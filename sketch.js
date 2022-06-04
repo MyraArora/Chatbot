d422f646e44aa35329c749de7ef6df7ff82961a6
MyraArora <89660720+MyraArora@users.noreply.github.com>
2022-06-05 03:09:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,24 @@
 function addChat(input,product){
   const messagesContainer=document.getElementById("messages");
-  let userDiv=document.createElement("div");
+  let userDiv=document.createElement("div")
   userDiv.id="user";
   userDiv.className="user response";
   userDiv.innerHTML=`<img src="https://cliply.co/wp-content/uploads/2019/09/391909180_THINKING_FACE_400px.gif" class="avatar"><span>${input}</span>`;
   messagesContainer.appendChild(userDiv);
-  let botDiv=document.createElement("div");
-  let botImg=document.createElement("img");
-  let botText=document.createElement("span");
-  botImg.src="bot-mini.png";
+  let botDiv=document.createElement("div")
+  let botImg=document.createElement("img")
+  let botText=document.createElement("span")
+  botImg.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpz5h49_1CBYw6MBGlw8CNX-_U-BxVkBMLfQ&usqp=CAU"
   botImg.className="avatar";
-  botDiv.className="bot response";
-  botDiv.id="bot";
-  botText.innerText="Typing...";
+  botDiv.className="bot response"
+  botText.innerText="Typing..."
   botDiv.appendChild(botText);
   botDiv.appendChild(botImg);
   messagesContainer.appendChild(botDiv);
-  messagesContainer.scrollTop = messagesContainer.scrollHeight-messagesContainer.clientHeight;
+  messagesContainer.scrollTop=messagesContainer.scrollHeight-messagesContainer.clientHeight;
   setTimeout(timeOut,2000);
   function timeOut(){
   botText.innerText=`${product}`;
-  textToSpeech(product);
 }
 }
 
@@ -33,28 +31,28 @@ document.addEventListener("DOMContentLoaded", () => {
       output(input);
     }
   });
+  document.getElementById("myBtn").addEventListener("click", function() {
+    let input = inputField.value;
+      inputField.value = "";
+      output(input);
+});
 });
 
 function output(input){
   let product;
-  let text=input.toLowerCase().replace(/[^\w\s]/gi,"").replace(/[\d]/gi,"").trim();
+  let text=input;
   // console.log("please please its r u".replace("please", "_").replace("please", " ").replace("its"," ").replace("r u","are you"));
   text=text.replace(/I feel/gi," ").replace(/whats/gi,"what is");
   text=text.replace(/please/gi," ").replace(/its/gi, " ").replace(/r u/gi, "are you");
-  console.log(text);
-  if (compare(prompts, replies, text)){
-    product=compare(prompts,replies,text);
-  }  else if (text.match(/thank/gi)){
-    product="You are welcome";
-  }  else if (text.match(/birthday/gi)){
-    product = "I was  created on 4th December 2021";
-  }  else if (text.match(/love/gi)){
-    product="As heart-breaking as it is, I have no feelings 😥";
-  }
-  else{
-    product=alternatives[Math.floor(Math.random()*alternatives.length)];
-  }
-  console.log(`-${product}`);
+  if (text<10){
+    product="Your CRP level is completely normal. ";
+  }  
+  if (text<27 & text>10){
+    product="You have mild inflamation. We advice you to consult a doctor immediately.";
+  }  
+  if (text>26){
+    product="Very high degree of inflamation. You need to consult a doctor urgently. This should be followed by an HRCT scan to determine the level of lung infection. Treatment with steroids may be needed on the advice of the doctor.";
+  }  
   addChat(input,product);
 }
 
